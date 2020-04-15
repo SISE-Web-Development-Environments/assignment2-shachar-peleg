eae171d503410c9867717dcee95065e50ad3be2e
shacharmeretz <shacharmeretz1@gmail.com>
2020-04-15 22:46:10 +0300

    storage

---
+++ storage.js
@@ -0,0 +1,65 @@
+
+function registerMember(user) {
+	localStorage.setItem(getUserName(user), user);
+}
+
+function createUser(pusername , pfullname , pemail , ppassword , pdateOfBirth) {
+	var user={username:pusername , fullname:pfullname , email:pemail , password:ppassword , dateOfBirth:pdateOfBirth};
+	return user;
+}
+
+function getUserName(user) {
+	return user.username;
+}
+
+function getPassword(user)
+{
+	return user.password;
+}
+
+function checkIfPasswordCorrect() {
+	let username=document.getElementById('user').value
+	let password=document.getElementById('password').value
+	alert("hey");
+	if(username=='p' && password=='p')
+	{
+		return show('game','login');
+	}
+	else
+	{
+	let newUser = localStorage.getItem(username);
+	if(newUser.getPassword()==password)
+	{
+		return show('game','login');
+	}
+}
+	alert("user name / password are wrong. try again")
+	return false;
+}
+
+function checkIfUserNameCorrect(username) {
+	var user = localStorage.getItem(username);
+	if(user!=null || username==p)
+		return true;
+	return false;
+}
+
+
+function show(shown, hidden1) {
+	document.getElementById(shown).style.display = 'block';
+	document.getElementById(hidden1).style.display = 'none';
+	return false;
+}
+function showall(shown, hidden1 , hidden2 , hidden3) {
+	document.getElementById(shown).style.display = 'block';
+	document.getElementById(hidden1).style.display = 'none';
+	document.getElementById(hidden2).style.display = 'none';
+	document.getElementById(hidden3).style.display = 'none';
+	return false;
+}
+function showandalert(shown, hidden1) {
+	alert("you sucssefuly register to the system")
+	document.getElementById(shown).style.display = 'block';
+	document.getElementById(hidden1).style.display = 'none';
+	return false;
+}
--- validation.js
+++ validation.js
@@ -1,66 +1,59 @@
-	$('form[id="signupForm"]').validate({
-		rules: {
+function validate() {
+	$('form[id="#signupForm"]').validate({
+	rules: {
 			fullname:
-			{
-				required:true,
-				accept:'[a-zA-Z]+'
-			},
+
+				{
+					required: true,
+					accept: '[a-zA-Z]+'
+				},
 			register_user:
-			{
-				required:true,
-				minlength:2
-			},
-			register_password:{
-				required:true,
-				minlength:5
+				{
+					required: true,
+					minlength: 2
+				},
+			register_password: {
+				required: true,
+				minlength: 6
 			},
 			register_confirmpassword: {
-				required:true,
-				minlength:6,
-				equalTo:"#password"
+				required: true,
+				minlength: 6,
+				equalTo: "#password"
 			},
 			email: {
-				required:true,
-				email:true
+				required: true,
+				email: true
 			},
 			birthdate: {
 
-                required: true
+				required: true
 
-            }
+			}
 		},
 		messages: {
-			fullname:"please enter your full name",
-			register_user:{
-				required:"please enter a username",
-				minlength:"your username must consist of at least 2 characters"
+			fullname: "please enter your full name",
+			register_user: {
+				required: "please enter a username",
+				minlength: "your username must consist of at least 2 characters"
 			},
-			register_password:{
-				required:"please provide a password",
-				minlength:"your password must be at least 6 characters long"
+			register_password: {
+				required: "please provide a password",
+				minlength: "your password must be at least 6 characters long"
 			},
-			register_confirmpassword:{
-				required:"please confirm your password",
-				equalTo:"please enter the same password as above"
+			register_confirmpassword: {
+				required: "please confirm your password",
+				equalTo: "please enter the same password as above"
 			}
-
-		},
-		submitHandler: function(form) {
-			form.submit();
-		  }
+		}
 	});
-
-$("username").focus(function(){
-	var fullname=$("#fullname").val();
-	if(fullname && !this.value)
-	{
-		this.value=fullname;
-	}
-});
-
-
-if ($("#signupForm").validate()) {
-
-	addUser(new User($("#fullname").val(), $("#username").val(), $("#password").val(), $("#remail").val(), $("#rbirth").val()));
-};
+}
+function focus() {
+	$("username").focus(function () {
+		var fullname = $("#fullname").val();
+		if (fullname && !this.value) {
+			this.value = fullname;
+		}
+	});
+}
 
